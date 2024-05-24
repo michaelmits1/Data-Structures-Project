5d763363c81b885b65b0b4133cf38b96ded62b60
michaelmits1 <mmitsainas@gmail.com>
2024-05-24 21:48:38 +0300

    v1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
-//
-// Created by mmitsainas on 5/24/24.
-//
+#include "region.h"
+using namespace std;
+
+int main() {
+    string fname = "/home/mmitsainas/datastrproj/Data-Structures-Project2/Data-Structures-Project/input.csv";
+    node *root=nullptr;
+    root=read_csv(root, fname);
+    cout << "Inorder traversal:\n";
+    traverseInOrder(root,0);
+
+    return 0;
+}
